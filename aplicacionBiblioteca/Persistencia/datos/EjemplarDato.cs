6e8865c27212efe92718d42d5e97ca94b796d3dd
ganazar <144313897+ganazar@users.noreply.github.com>
2025-11-19 20:50:02 +0100

    Creación tabla y entity

---
+++ aplicacionBiblioteca/Persistencia/datos/EjemplarDato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Datos
+{
+    internal class EjemplarDato : Entity<string>
+    {
+        public EjemplarDato(string codigo) : base(codigo)
+        {
+
+        }
+    }
+}
---
+++ aplicacionBiblioteca/Persistencia/datos/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Datos
+{
+    internal class Entity<T>
+    {
+        private T id;
+        public Entity (T id){
+            this.id = id;
+        }
+        public T Id
+        {
+            get {  return id; }
+        }
+    }
+}
