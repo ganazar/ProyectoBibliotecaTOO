8a782c20c62574992589022fa8147615dd71c87c
papellej <pabpelle@gmail.com>
2025-11-26 19:19:33 +0100

    Interfaces

--- aplicacionBiblioteca/Persistencia/Interfaces/IPersistenciaAdquisiciones.cs
+++ aplicacionBiblioteca/Persistencia/Interfaces/IPersistenciaAdquisiciones.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Persistencia
+namespace Persistencia.Interfaces
 {
-    public class Class1
+    internal interface IPersistenciaAdquisiciones
     {
     }
 }
---
+++ aplicacionBiblioteca/Persistencia/Interfaces/IPersistenciaSala.cs
@@ -0,0 +1,41 @@
+﻿using modeloDominio;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Interfaces
+{
+    public interface IPersistenciaSala
+    {
+        /// <summary>
+        /// Añade el usuario a la base de datos si no existe todavia
+        /// </summary>
+        /// <param name="user">Usuario a insertar</param>
+        /// <returns>True si se ha añadido correctamente false en caso contrario</returns>
+        bool CreateUsuario(Usuario user);
+
+        /// <summary>
+        /// Lee el usuario de base de datos
+        /// </summary>
+        /// <param name="user">Usuario con la clave</param>
+        /// <returns>Devuelve el usuario si existe en la base de datos, nulo en caso contrario</returns>
+        Usuario ReadUsuario(Usuario user);
+
+        /// <summary>
+        /// Actualiza el usuario si existe en la base de datos, si no lo encuentra no hace nada
+        /// </summary>
+        /// <param name="user">Usuario a actualizar</param>
+        /// <returns>True si actualiza el usuario false en caso contrario</returns>
+        bool UpdateUsuario(Usuario user);
+
+        /// <summary>
+        /// Borra el usuario a la base de datos si existe
+        /// </summary>
+        /// <param name="user">Usuario a borrar</param>
+        /// <returns>True si se ha borrado correctamente false en caso contrario</returns>
+        bool DeleteUsuario(Usuario user);
+    }
+}
