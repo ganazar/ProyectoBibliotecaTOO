6a1b40c8fb8eed35dabc491ce3be2ef706f415ad
papellej <pabpelle@gmail.com>
2025-11-26 20:54:31 +0100

    .

--- aplicacionBiblioteca/Persistencia/datos/PrestamoDato.cs
+++ aplicacionBiblioteca/Persistencia/datos/PrestamoDato.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using modeloDominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,27 @@ using System.Threading.Tasks;
 
 namespace Persistencia.Datos
 {
-    internal class PrestamoDato
+    internal class ClavePrestamo
     {
+        private string dni;
+        private DateTime fecha;
+        public ClavePrestamo(string dni, DateTime fecha)
+        {
+            this.dni = dni;
+            this.fecha = fecha;
+        }
+        public string Dni { get { return this.dni; } }
+        public DateTime Fecha { get { return this.fecha; } }
     }
+
+    internal class PrestamoDato : Entity<ClavePrestamo>
+    {
+        private EstadoPrestamo estado;
+        public PrestamoDato(ClavePrestamo id, EstadoPrestamo estado) : base(id)
+        {
+            this.estado = estado;
+        }
+        public EstadoPrestamo Estado { get { return this.estado; } }
+    }
+
 }
