14ce9700ecc27c9c038648c6905d7212cd56075f
agamilbu <agamilbu@unirioja.es>
2025-11-26 19:20:46 +0100

    PersonalDato

---
+++ aplicacionBiblioteca/Persistencia/datos/PersonalDato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia.Datos
+{
+    internal class PersonalDato: Entity<string>
+    {
+        public PersonalDato(string NSS) : base(NSS) { 
+             
+        
+        }
+    }
+}
