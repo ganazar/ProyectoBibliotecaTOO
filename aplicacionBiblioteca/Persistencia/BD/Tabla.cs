069c8442b8714ffbe15880639172fde3a43ac407
agamilbu <agamilbu@unirioja.es>
2025-11-26 19:58:16 +0100

    Creacion ObjetosDato

--- aplicacionBiblioteca/Persistencia/BD/Tabla.cs
+++ aplicacionBiblioteca/Persistencia/BD/Tabla.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Persistencia.Datos
 {
-    internal class Tabla
+    internal class Tabla<T,U>: KeyedCollection<T,U> where U : Entity<T>
     {
+        protected override T GetKeyForItem(U u)
+        {
+            return u.Id;
+        }
     }
 }
