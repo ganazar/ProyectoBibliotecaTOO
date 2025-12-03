893014b8b1ac4304a6058c8f2962c9524f8fa442
agamilbu <agamilbu@unirioja.es>
2025-12-03 20:43:18 +0100

    Clase Transformers.cs

--- aplicacionBiblioteca/Persistencia/BD/Transformers.cs
+++ aplicacionBiblioteca/Persistencia/BD/Transformers.cs
@@ -12,7 +12,7 @@ namespace Persistencia.BD
     {
         public static Audiolibro Audiolibro(AudiolibroDato a)
         {
-            return new Audiolibro(a.Id,a.Duracion,a.Formato);
+            return new Audiolibro(a.Id,a.Titulo, a.Autor,a.Editorial,a.AñoEdicion,a.Duracion,a.Formato);
         }
 
         public static AudiolibroDato AudiolibroDato(Audiolibro a)
@@ -20,35 +20,64 @@ namespace Persistencia.BD
             return new AudiolibroDato(a.ISBN,a.Titulo,a.Autor,a.Editorial,a.AñoEdicion,a.Formato,a.Duracion);
         }
 
-        public static Documento Documento(DocumentoDato d)
+        public static Ejemplar Ejemplar(EjemplarDato e, Documento d)
         {
-            return new Documento();
-        }
-
-        public static DocumentoDato DocumentoDato(Documento d) {
-            return new DocumentoDato();
-        }
-
-        public static Ejemplar Ejemplar(EjemplarDato e)
-        {
-            return new Ejemplar();
+            return new Ejemplar(d);
         }
 
         public static EjemplarDato EjemplarDato(Ejemplar e)
         {
-            return new EjemplarDato();
+            return new EjemplarDato(e.CodEjemplar,e.Prestado, e.Doc.ISBN);
         }
 
         public static FisicoDato FisicoDato(Fisico f)
         {
-            return new FisicoDato();
+            return new FisicoDato(f.ISBN,f.Titulo,f.Autor,f.Editorial,f.AñoEdicion);
         }
 
         public static Fisico Fisico(FisicoDato f)
         {
-            return new modeloDominio.Fisico();
+            return new Fisico(f.Id,f.Titulo,f.Autor,f.Editorial,f.AñoEdicion);
         }
 
-        public s
+        public static PersonalAdquisicionDato PersonalAdquisicionDato(PersonalAdquisiciones p)
+        {
+            return new PersonalAdquisicionDato(p.NSS, p.Nombre);
+        }
+
+        public static PersonalAdquisiciones PersonalAdquisiciones(PersonalAdquisicionDato p)
+        {
+            return new PersonalAdquisiciones(p.Id, p.Nombre);
+        }
+
+        public static PersonalSalaDato PersonalSalaDato(PersonalSala p)
+        {
+            return new PersonalSalaDato(p.NSS, p.Nombre);
+        }
+
+        public static PersonalSala PersonalSala(PersonalSalaDato p)
+        {
+            return new PersonalSala(p.Id, p.Nombre);
+        }
+
+        public static PrestamoDato PrestamoDato(Prestamo p)
+        {
+            return new PrestamoDato(new ClavePrestamo(p.Usuario.DNI, p.FechaPrestamo), p.Estado);
+        }
+
+        public static Prestamo Prestamo(PrestamoDato p)
+        {
+            return new Prestamo(new Usuario(p.Id.Dni), p.Id.Fecha);
+        }
+
+        public static UsuarioDato UsuarioDato(Usuario u)
+        {
+            return new UsuarioDato(u.DNI, u.Nombre);
+        }
+
+        public static Usuario Usuario(UsuarioDato u)
+        {
+            return new Usuario(u.Id, u.Nombre);
+        }
     }
 }
