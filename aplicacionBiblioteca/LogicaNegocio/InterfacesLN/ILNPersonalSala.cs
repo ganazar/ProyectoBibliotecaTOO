bc0b793eb5e670245a4298b0b27d79518014c06e
agamilbu <agamilbu@unirioja.es>
2025-12-10 17:53:46 +0100

    Metodos de las interfaces a medias

--- aplicacionBiblioteca/LogicaNegocio/InterfacesLN/ILNPersonalSala.cs
+++ aplicacionBiblioteca/LogicaNegocio/InterfacesLN/ILNPersonalSala.cs
@@ -3,10 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using modeloDominio;
+using Persistencia;
 
 namespace LogicaNegocio.InterfacesLN
 {
     internal interface ILNPersonalSala
     {
+        /// <summary>
+        /// Obtiene una lista de los ejemplares que un usuario tiene actualmente en prestamo.
+        /// </summary>
+        /// <param name="u">El usuario del cual se quieren consultar los ejemplares en prestamo.</param>
+        /// <returns>Una lista de objetos <see cref="Ejemplar"/> prestados al usuario.</returns>
+        List<Ejemplar> consultarEjemplaresPrestadosUsuario(Usuario u);
+
+        /// <summary>
+        /// Consulta los préstamos que están activos asociados a un usuario.
+        /// </summary>
+        /// <param name="u">El usuario cuyos préstamos activos se desean consultar.</param>
+        /// <returns>Una lista de objetos <see cref="Prestamo"/> que no han finalizado.</returns>
+        List<Prestamo> consultarPrestamosNoDevueltos(Usuario u);
+
+        /// <summary>
+        /// Verifica si existe algún ejemplar disponible para préstamo de un documento específico.
+        /// </summary>
+        /// <param name="d">El documento que se desea verificar.</param>
+        /// <returns><c>true</c> si hay al menos un ejemplar disponible; de lo contrario, <c>false</c>.</returns>
+        bool consultarDisponibilidad(Documento d);
+
+        /// <summary>
+        /// Calcula la primera fecha en la que un ejemplar del documento volverá a estar disponible.
+        /// </summary>
+        /// <param name="d">El documento sobre el cual se consulta la fecha.</param>
+        /// <returns>Un objeto <see cref="DateTime"/> indicando la fecha de disponibilidad más próxima.</returns>
+        DateTime fechaDisponible(Documento d);
+
+        /// <summary>
+        /// Identifica el documento que ha sido prestado más veces.
+        /// </summary>
+        /// <returns>El objeto <see cref="Documento"/> con mayor número de prestamos(contando todos sus ejemplares).</returns>
+        Documento masLeido();
+
+        /// <summary>
+        /// Registra un nuevo préstamo de un ejemplar a un usuario.
+        /// </summary>
+        /// <param name="e">El ejemplar específico que será prestado.</param>
+        /// <param name="u">El usuario que solicita el préstamo.</param>
+        /// <returns>El objeto <see cref="Prestamo"/> generado.</returns>
+        Prestamo iniciarPrestamo(Ejemplar e, Usuario u);
+
+        /// <summary>
+        /// Finaliza el préstamo asociado a un ejemplar, marcándolo como devuelto.
+        /// </summary>
+        /// <param name="e">El ejemplar que está siendo devuelto a la sala.</param>
+        void devolverPrestamo(Ejemplar e);
+
+        /// <summary>
+        /// Obtiene el usuario asociado a un préstamo específico.
+        /// </summary>
+        /// <param name="p">El préstamo del cual se quiere conocer el titular.</param>
+        /// <returns>El objeto <see cref="Usuario"/> asociado al préstamo.</returns>
+        Usuario usuarioDePrestamo(Prestamo p);
+
+        /// <summary>
+        /// Consulta el estado actual de un préstamo (enProceso,Finalizado).
+        /// </summary>
+        /// <param name="p">El préstamo a verificar.</param>
+        /// <returns>Un valor de tipo <see cref="EstadoPrestamo"/>.</returns>
+        EstadoPrestamo estadoPrestamo(Prestamo p);
+
+        /// <summary>
+        /// Obtiene todos los ejemplares de la biblioteca que están en préstamo.
+        /// </summary>
+        /// <returns>Una lista de <see cref="Ejemplar"/> no devueltos.</returns>
+        List<Ejemplar> ejemplaresNoDevueltos();
+
+        /// <summary>
+        /// Obtiene el listado de préstamos asociados a un documento específico.
+        /// </summary>
+        /// <param name="d">El documento del cual se buscan los préstamos.</param>
+        /// <returns>Una lista de objetos <see cref="Prestamo"/> relacionados con el documento.</returns>
+        List<Prestamo> PrestamosDocumento(Documento d);
+
+        /// <summary>
+        /// Obtiene todos los préstamos que están activos en el sistema actualmente.
+        /// </summary>
+        /// <returns>Una lista de objetos <see cref="Prestamo"/> en curso.</returns>
+        List<Prestamo> PrestamosEnProceso();
+
+        /// <summary>
+        /// Obtiene todos los préstamos cuya fecha de devolución ha expirado.
+        /// </summary>
+        /// <returns>Una lista de objetos <see cref="Prestamo"/> vencidos.</returns>
+        List<Prestamo> PrestamosVencidos();
     }
 }
--- aplicacionBiblioteca/LogicaNegocio/LN/LNPersonalSala.cs
+++ aplicacionBiblioteca/LogicaNegocio/LN/LNPersonalSala.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LogicaNegocio.InterfacesLN;
+using modeloDominio;
+using Persistencia.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,125 @@ using System.Threading.Tasks;
 
 namespace LogicaNegocio
 {
-    public class LNPersonalSala
+    public class LNPersonalSala : ILNPersonalSala
     {
+        List<Prestamo> allPrestamos; //solo para comprobar
+        
+        /// <summary>
+        /// Obtiene una lista de los ejemplares que un usuario tiene actualmente en prestamo.
+        /// </summary>
+        /// <param name="u">El usuario del cual se quieren consultar los ejemplares en prestamo.</param>
+        /// <returns>Una lista de objetos <see cref="Ejemplar"/> prestados al usuario.</returns>
+        List<Ejemplar> consultarEjemplaresPrestadosUsuario(Usuario u)
+        { //List<Ejemplar> l = new List<Ejemplar>();
+
+            //foreach (Prestamo p in PersistenciaSala.GetAllPrestamos())
+            //{
+            //    if (p.Usuario.DNI == u.DNI)
+            //    {
+            //        foreach (Ejemplar e in p.Ejemplares)
+            //        {
+            //            l.Add(e);
+            //        }
+            //    }
+            //}
+
+            return allPrestamos.Where(p => p.Usuario.DNI == u.DNI).SelectMany(p => p.Ejemplares).ToList();
+        }
+
+        /// <summary>
+        /// Consulta los préstamos que están activos asociados a un usuario.
+        /// </summary>
+        /// <param name="u">El usuario cuyos préstamos activos se desean consultar.</param>
+        /// <returns>Una lista de objetos <see cref="Prestamo"/> que no han finalizado.</returns>
+        List<Prestamo> consultarPrestamosNoDevueltos(Usuario u)
+        {
+            return allPrestamos.Where(p => p.Usuario.DNI == u.DNI).Where(p => p.Estado == EstadoPrestamo.EnProceso).ToList();
+        }
+
+        /// <summary>
+        /// Verifica si existe algún ejemplar disponible para préstamo de un documento específico.
+        /// </summary>
+        /// <param name="d">El documento que se desea verificar.</param>
+        /// <returns><c>true</c> si hay al menos un ejemplar disponible; de lo contrario, <c>false</c>.</returns>
+        public bool consultarDisponibilidad(Documento d)
+        {
+            return allPrestamos.SelectMany(p => p.Ejemplares).Where(e => e.Doc == d).Any(e => e.Prestado == false);
+        }
+
+        /// <summary>
+        /// Calcula la primera fecha en la que un ejemplar del documento volverá a estar disponible.
+        /// </summary>
+        /// <param name="d">El documento sobre el cual se consulta la fecha.</param>
+        /// <returns>Un objeto <see cref="DateTime"/> indicando la fecha de disponibilidad más próxima.</returns>
+        DateTime fechaDisponible(Documento d)
+        {
+            List<Ejemplar> ej = allPrestamos.SelectMany(p => p.Ejemplares).Where(e => e.Doc == d).ToList();
+            
+            foreach (Ejemplar e in p.Ejemplares)
+            {
+                
+            }
+        }
+
+        /// <summary>
+        /// Identifica el documento que ha sido prestado más veces.
+        /// </summary>
+        /// <returns>El objeto <see cref="Documento"/> con mayor número de prestamos(contando todos sus ejemplares).</returns>
+        Documento masLeido();
+
+        /// <summary>
+        /// Registra un nuevo préstamo de un ejemplar a un usuario.
+        /// </summary>
+        /// <param name="e">El ejemplar específico que será prestado.</param>
+        /// <param name="u">El usuario que solicita el préstamo.</param>
+        /// <returns>El objeto <see cref="Prestamo"/> generado.</returns>
+        Prestamo iniciarPrestamo(Ejemplar e, Usuario u);
+
+        /// <summary>
+        /// Finaliza el préstamo asociado a un ejemplar, marcándolo como devuelto.
+        /// </summary>
+        /// <param name="e">El ejemplar que está siendo devuelto a la sala.</param>
+        void devolverPrestamo(Ejemplar e);
+
+        /// <summary>
+        /// Obtiene el usuario asociado a un préstamo específico.
+        /// </summary>
+        /// <param name="p">El préstamo del cual se quiere conocer el titular.</param>
+        /// <returns>El objeto <see cref="Usuario"/> asociado al préstamo.</returns>
+        Usuario usuarioDePrestamo(Prestamo p);
+
+        /// <summary>
+        /// Consulta el estado actual de un préstamo (enProceso,Finalizado).
+        /// </summary>
+        /// <param name="p">El préstamo a verificar.</param>
+        /// <returns>Un valor de tipo <see cref="EstadoPrestamo"/>.</returns>
+        EstadoPrestamo estadoPrestamo(Prestamo p);
+
+        /// <summary>
+        /// Obtiene todos los ejemplares de la biblioteca que están en préstamo.
+        /// </summary>
+        /// <returns>Una lista de <see cref="Ejemplar"/> no devueltos.</returns>
+        List<Ejemplar> ejemplaresNoDevueltos();
+
+        /// <summary>
+        /// Obtiene el listado de préstamos asociados a un documento específico.
+        /// </summary>
+        /// <param name="d">El documento del cual se buscan los préstamos.</param>
+        /// <returns>Una lista de objetos <see cref="Prestamo"/> relacionados con el documento.</returns>
+        List<Prestamo> PrestamosDocumento(Documento d);
+
+        /// <summary>
+        /// Obtiene todos los préstamos que están activos en el sistema actualmente.
+        /// </summary>
+        /// <returns>Una lista de objetos <see cref="Prestamo"/> en curso.</returns>
+        List<Prestamo> PrestamosEnProceso();
+
+        /// <summary>
+        /// Obtiene todos los préstamos cuya fecha de devolución ha expirado.
+        /// </summary>
+        /// <returns>Una lista de objetos <see cref="Prestamo"/> vencidos.</returns>
+        List<Prestamo> PrestamosVencidos();
+
     }
 }
